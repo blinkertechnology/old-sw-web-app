ef59c9603aa5a27b69a66b5c6750849fae94ecc2
Wouter De Loose <wouter@woutr.me>
2023-01-16 16:54:51 +0800

    Add https to staging env

--- .env.staging.js
+++ .env.staging.js
@@ -10,7 +10,7 @@ module.exports = {
     'VUE_APP_VERSION': parsed['version'],
 
     'VUE_APP_TITLE': 'Sorted Wallet',
-    'VUE_APP_URL': 'http://staging-api.sorted.finance',
+    'VUE_APP_URL': 'https://staging-api.sorted.finance',
 
     'VUE_APP_KAI_AD_PUBLISHER_ID': 'bc1327d6-abc0-427e-be35-c1e9f5c4ec57',
     'VUE_APP_KAI_AD_APP': 'sorted-wallet',
