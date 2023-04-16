d7ae6c056477a54b145f12deedb8eea98bd4f3d2
Anirudhsfc <agarwalanirudh14@gmail.com>
2023-04-17 03:28:04 +0800

    Changed landing page name

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import FirstPage from '../components/FirstPage'
+import LanguageSetupPage from '../components/LanguageSetupPage'
 import TermsAndConditions from '../components/TermsAndConditions';
 import HomePage from "@/components/HomePage";
 import forgotpass from "@/components/Auth/ForgotPass";
@@ -27,14 +27,14 @@ const router = new Router({
   routes: [
     {
       path: "/",
-      name: "FirstPage",
-      component: FirstPage
+      name: "LanguageSetupPage",
+      component: LanguageSetupPage
     },
-    {
-      path: "/first",
-      name: "FirstPage",
-      component: FirstPage
-    },
+    // {
+    //   path: "/first",
+    //   name: "FirstPage",
+    //   component: FirstPage
+    // },
     {
       path: "/terms",
       name: "TermsAndConditions",
