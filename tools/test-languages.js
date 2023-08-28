f473bf7c99e9884019da55a3263300a6fd10f557
Wouter De Loose <wouter@woutr.me>
2023-08-28 12:46:05 +0800

    Add language test

---
+++ tools/test-languages.js
@@ -0,0 +1,21 @@
+const { join, extname} = require('path');
+const { readdirSync, readFileSync } = require('fs');
+const core = require('@actions/core');
+
+/**
+ * Run through all the language files and validate that they're correct JSON
+ */
+const langDir = join(process.cwd(), 'src', 'lang');
+const langFiles = readdirSync(langDir).filter(function(file) {
+    return extname(file) === '.json';
+})
+
+langFiles.forEach(function(file) {
+    const data = readFileSync(join(langDir, file), 'utf-8');
+
+    try {
+        JSON.parse(data);
+    } catch(err) {
+        core.setFailed(`Failed to parse ${file}`);
+    }    
+})
